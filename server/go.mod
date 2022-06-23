03c9c14f559930c1d6285fc0339d8b5477164a88
Anirudh-rao <anirudh131299@gmail.com>
2022-06-23 22:41:11 +0530

    Added Basic Routes

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module github.com/Anirudh-rao/CalorieTracker-Go-React
 
 go 1.18
+
+require go.mongodb.org/mongo-driver v1.9.1
--- server/main.go
+++ server/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/Anirudh-rao/CalorieTracker-Go-React/routes"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	router := gin.New()
+	//Gin.logger will provide us with a Middleware to monitor the Website
+	router.Use(gin.Logger())
+	router.Use(cors.Default())
+
+	router.POST("/entry/create", routes.AddEntry)
+	router.GET("/entries", routes.Getentries)
+	router.GET("/entry/:id/", routes.GetentryById)
+	router.GET("/ingredient/:ingredient", routes.GetEntriesByIngredient)
+
+	router.PUT("/entry/update/:id", routes.UpdateEntry)
+	router.PUT("/ingredient/update/:id",routes.UpdateIngredient)
+	router.DELETE("/entry/delete/:id",  routes.DeleteEntry)
+
+	router.Run(":"+port)
+}
