e26d37c4d17eeca57f25fb4f4132d0210e7a1d0a
Anirudh-rao <anirudh131299@gmail.com>
2022-07-16 23:46:22 +0530

    updates in Entriers

--- frontend/src/components/entries.components.js
+++ frontend/src/components/entries.components.js
@@ -1,4 +1,57 @@
-import React , { useState, useEffect } from 'react';
+import React , { useState, useEffect, setRefreshData  } from 'react';
 import axios from "axios";
 import { Button, Form , Container, Modal } from 'bootstrap';
 import Entry from './single-entry.component';
+
+const Entriess=() =>{
+    return(
+        <div>
+            <Container>
+                <button onClick={()=> setAddNewEntry(true)}>Track Today's</button>
+            </Container>
+            <Container>
+                {entriers!=null && entriers.map((entry, i)=>
+                    (
+                        <Entry  entryData={entry} deleteSingleEntry={deleteSingleEntry}
+                                setChangeIngredient={setChangeIngredient} setChangeEntry={setChangeEntry}
+                        />
+
+                    )
+                )}
+            </Container>
+        </div>
+    );
+    
+}
+
+function addSingleEntry(){
+    setAddNewEntry(false)
+    //Connecting to Our Backend
+    var url = "https://localhost:8080/entry/create"
+    //Mapping our data using axios
+    axios.post(url,{
+        "ingredients":newEntry.ingredients,
+        "dish": newEntry.dish,
+        "calories":newEntry.calories,
+        "fat":parseFloat(newEntry.fat)
+    }).then(response =>{
+        if(response.status
+            ==200){
+                setRefreshData(true)
+            }
+    })
+
+}
+
+function deleteSingleEntry(id){
+        var url = "https://localhost:8080/entry/delete"+id
+        axios.delete(url,{
+            
+        }).then(response =>{
+            if(response.status
+                ==200){
+                    setRefreshData(true)
+                }
+        })
+
+}
